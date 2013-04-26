5b2a22554187cf75dd71f88f779b6e53a64dcf5a
John Andrews <john.m.andrews@gmail.com>
2013-04-26 17:15:44 -0400

    Add license info and url for clojars promotion

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject dieter/ember "0.4.0"
   :description "Compile handlebars templates in a way compatible with Ember.js"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://github.com/edgecase/dieter-ember"
   :dependencies [[dieter "0.4.0"]])
